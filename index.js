d0da442768bf164f9166ee6c508f9b015399a442
Daniel Minshew <ofenixculpa@gmail.com>
2015-12-26 18:53:03 -0600

    add test for counter

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // HI!
+var expect = require('expect')
+
+// reducer
+
+function counter(state, action) {
+  if (action.type === 'INCREMENT') {
+    return state + 1;
+  } else if (action.type === 'DECREMENT') {
+    return state - 1;
+  }
+}
+
+
+// tests for function counter()
+
+expect(
+  counter(0, { type: 'INCREMENT'})
+).toEqual(1);
+
+expect(
+  counter(1, { type: 'INCREMENT'})
+).toEqual(2);
+
+expect(
+  counter(2, { type: 'DECREMENT'})
+).toEqual(1);
+
+expect(
+  counter(1, { type: 'DECREMENT'})
+).toEqual(0);
+
+console.log('Tests passed!')
